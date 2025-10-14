6f3dfba688dda4c412ef1b75dd0041f47267ecaf
LLosTy <MarcoJurgenJuras@gmail.com>
2025-10-14 15:00:31 +0200

    chore(page): changed layout style

--- app/page.js
+++ app/page.js
@@ -196,9 +196,11 @@ export default function Home() {
     // <div>
 
     <div className="font-sans grid grid-rows items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2"></main>
       <TableDemo users={users} onAddUser={addUser} onModifyUser={modifyUser} />
-      <Barcode users={users} />
+
+      <main className="flex flex-col gap-[32px] row-start-2">
+        <Barcode users={users} />
+      </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
